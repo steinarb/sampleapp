6ce0570b29a506137943a1276398f72b3feb96ca
Steinar Bang <sb@dod.no>
2022-09-22 21:27:19 +0200

    Remove unnecessary eq() in mockito when().thenReturn()

--- sampleapp.web.api/src/test/java/no/priv/bang/sampleapp/web/api/SampleappWebApiTest.java
+++ sampleapp.web.api/src/test/java/no/priv/bang/sampleapp/web/api/SampleappWebApiTest.java
@@ -438,7 +438,7 @@ class SampleappWebApiTest extends ShiroTestBase {
     private ServletConfig createServletConfigWithApplicationAndPackagenameForJerseyResources() {
         ServletConfig config = mock(ServletConfig.class);
         when(config.getInitParameterNames()).thenReturn(Collections.enumeration(Arrays.asList(ServerProperties.PROVIDER_PACKAGES)));
-        when(config.getInitParameter(eq(ServerProperties.PROVIDER_PACKAGES))).thenReturn("no.priv.bang.sampleapp.web.api.resources");
+        when(config.getInitParameter(ServerProperties.PROVIDER_PACKAGES)).thenReturn("no.priv.bang.sampleapp.web.api.resources");
         ServletContext servletContext = mock(ServletContext.class);
         when(servletContext.getContextPath()).thenReturn("/sampleapp");
         when(config.getServletContext()).thenReturn(servletContext);
