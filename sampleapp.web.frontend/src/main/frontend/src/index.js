630858b9ef22e2631db67d7e0cec6b7ab73cdfe3
Steinar Bang <sb@dod.no>
2024-01-07 16:52:06 +0100

    Make configureStore middleware argument a lambda

--- sampleapp.web.frontend/src/main/frontend/src/index.js
+++ sampleapp.web.frontend/src/main/frontend/src/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, Tuple } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import { Provider } from 'react-redux';
 import { createReduxHistoryContext } from "redux-first-history";
@@ -28,10 +28,7 @@ const {
 } = createReduxHistoryContext({ history: createBrowserHistory(), basename });
 const store = configureStore({
     reducer: createRootReducer(routerReducer),
-    middleware: [
-        sagaMiddleware,
-        routerMiddleware,
-    ],
+    middleware: () => new Tuple(sagaMiddleware, routerMiddleware),
 });
 sagaMiddleware.run(rootSaga);
 const history = createReduxHistory(store);
