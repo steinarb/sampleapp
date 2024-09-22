9fefbeb90de13d8382154677c399362127c880bb
Steinar Bang <sb@dod.no>
2024-09-22 20:18:38 +0200

    Add vite config file

---
+++ sampleapp.web.frontend/src/main/frontend/vite.config.js
@@ -0,0 +1,27 @@
+import eslint from 'vite-plugin-eslint';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    minified: false,
+    plugins: [eslint()],
+    build: {
+        manifest: true,
+        rollupOptions: {
+            // overwrite default .html entry
+            input: 'src/index.js',
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        },
+        // Relative to the root
+        outDir: '../../../target/classes',
+    },
+    // Treat .js files as jsx
+    esbuild: {
+        include: /\.js$/,
+        exclude: [],
+        loader: 'jsx',
+    },
+});
