563ade12ca25815a4ec38f7808aac497a0b5cbf0
Steinar Bang <sb@dod.no>
2024-12-23 21:08:40 +0100

    Change npm dependency react-router-dom 6.23.1 to react-router 7.1.0

--- sampleapp.web.frontend/src/main/frontend/src/App.js
+++ sampleapp.web.frontend/src/main/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router';
 import { HistoryRouter as Router } from "redux-first-history/rr6";
 import './App.css';
 import Home from './components/Home';
--- sampleapp.web.frontend/src/main/frontend/src/components/Home.js
+++ sampleapp.web.frontend/src/main/frontend/src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router';
 import { useSelector, useDispatch } from 'react-redux';
 import Container from './bootstrap/Container';
 import { LOGOUT_REQUEST } from '../reduxactions';
