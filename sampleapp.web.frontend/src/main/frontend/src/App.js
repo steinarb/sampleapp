a43decd25f0ba07a51da017b63807bec640407a6
Steinar Bang <sb@dod.no>
2024-12-23 23:24:13 +0100

    Convert component App from class to functional component

--- sampleapp.web.frontend/src/main/frontend/src/App.js
+++ sampleapp.web.frontend/src/main/frontend/src/App.js
@@ -7,21 +7,17 @@ import Counter from './components/Counter';
 import Login from './components/Login';
 import Unauthorized from './components/Unauthorized';
 
-class App extends Component {
-    render() {
-        const { history, basename } = this.props;
+export default function App(props) {
+    const { history, basename } = props;
 
-        return (
-            <Router history={history} basename={basename}>
-                <Routes>
-                    <Route exact path="/" element={<Home/>} />
-                    <Route exact path="/counter" element={<Counter/>} />
-                    <Route exact path="/login" element={<Login/>} />
-                    <Route exact path="/unauthorized" element={<Unauthorized/>} />
-                </Routes>
-            </Router>
-        );
-    }
+    return (
+        <Router history={history} basename={basename}>
+            <Routes>
+                <Route exact path="/" element={<Home/>} />
+                <Route exact path="/counter" element={<Counter/>} />
+                <Route exact path="/login" element={<Login/>} />
+                <Route exact path="/unauthorized" element={<Unauthorized/>} />
+            </Routes>
+        </Router>
+    );
 }
-
-export default App;
